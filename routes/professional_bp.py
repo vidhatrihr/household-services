5b50c9ba632a055d80f53eeebcb0d19f76a54646
vidhatrihr <167363972+vidhatrihr@users.noreply.github.com>
2024-11-14 10:35:59 +0530

    prof. search WIP

--- routes/professional_bp.py
+++ routes/professional_bp.py
@@ -1,17 +1,13 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect, request
 from models import *
 from flask_login import current_user
+from datetime import datetime
+
 
 professional_bp = Blueprint('professional', __name__)
 
 
-@professional_bp.route('/professional/accept-request/<int:request_id>')
-def professional_accept_request(request_id):
-  request = ServiceRequest.query.filter_by(id=request_id).first()
-  request.status = 'accepted'
-  request.professional = current_user.professional
-  db.session.commit()
-  return redirect('/')
+# ====== home ======
 
 
 @professional_bp.route('/professional/home')
@@ -44,11 +40,59 @@ def professional_home():
   )
 
 
+# ====== search ======
+
+
 @professional_bp.route('/professional/search')
 def professional_search():
   return render_template('professional_search.html')
 
 
+@professional_bp.route('/professional/search-results/<search_type>')
+def professional_search_results(search_type):
+  """ 
+  example url:
+    http://localhost:5000/professional/search-results/services?booking_date=yyyy-mm-dd&pin_code=...&service_id=...
+  """
+  if search_type == 'accepted-requests':
+    # get search parameters from query string in url (booking_date=yyyy-mm-dd&pin_code=...&service_id=...)
+    booking_date = request.args.get('booking_date')
+    pin_code = request.args.get('pin_code')
+    service_id = request.args.get('service_id')
+
+    # make an empty query
+    query = ServiceRequest.query
+
+    # apply filters as required
+    query = query.filter(ServiceRequest.status == 'accepted')
+    if booking_date:
+      # convert `booking_date str` to `datetime obj`
+      y, m, d = booking_date.split('-')
+      query = query.filter(ServiceRequest.booking_date == datetime(y, m, d))
+    if pin_code:
+      query = ...
+    if service_id:
+      query = ...
+
+    # fetch all rows
+    query = query.all()
+    return render_template('professional_search_results.html')
+
+# ====== summary ======
+
+
 @professional_bp.route('/professional/summary')
 def professional_summary():
   return render_template('professional_summary.html')
+
+
+# ====== accept-request ======
+
+
+@professional_bp.route('/professional/accept-request/<int:request_id>')
+def professional_accept_request(request_id):
+  request = ServiceRequest.query.filter_by(id=request_id).first()
+  request.status = 'accepted'
+  request.professional = current_user.professional
+  db.session.commit()
+  return redirect('/')
